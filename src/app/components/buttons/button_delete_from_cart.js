ac743d506439fcd82f66f04e2c3e85d0cf60c297
Jay Surya <raj.jaysurya04@gmail.com>
2024-08-18 13:40:56 +0530

    change name of button remove from cart to remove item

--- src/app/components/buttons/button_delete_from_cart.js
+++ src/app/components/buttons/button_delete_from_cart.js
@@ -14,7 +14,7 @@ const Button_Delete_From_Cart = ({ product_id }) => {
             className="flex items-center justify-center w-full bg-red-500 text-white font-semibold py-2 px-4 rounded-md shadow-md hover:bg-red-600 hover:shadow-lg focus:outline-none focus:ring-2 focus:ring-red-300 focus:ring-opacity-50 transition duration-300 ease-in-out"
             onClick={handleDeleteFromCart}
         >
-            Remove From Cart
+            Remove Item
         </button>
     );
 }
